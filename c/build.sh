40564417dedd72016eb2e0bba0f0d434363ea582
Pongsatorn <ton11797@hotmail.com>
2020-05-24 18:15:18 +0700

    clear

---
+++ c/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -e
+gcc stack0.c -o stack0
+gcc stack1.c -o stack1
+gcc stack2.c -o stack2
+gcc timeRNG.c -o Time
--- c/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh -e
-socat TCP-LISTEN:23232,reuseaddr,fork EXEC:./timeRNG
